7a8b0d0e7d2c18b512f4d297916c6ee143ac1a34
Student <student@Admins-Mac-mini-2.local>
2016-08-31 16:21:22 -0600

    Penultimate push

---
+++ public/scripts/sounds.js
@@ -0,0 +1,11 @@
+var win = new Audio ();
+win.src ="sounds/winbattle.wav"
+
+var levelUp = new Audio ();
+levelUp.src ="sounds/levelup.wav"
+
+var murloc = new Audio ();
+murloc.src ="sounds/murloc.mp3"
+
+var lazor = new Audio ();
+lazor.src ="sounds/lazor.wav"
